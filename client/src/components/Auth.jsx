6bfbec18eb5ceff5623c1cd929519b32e824eba0
Mayra Zurita <mizrmx@gmail.com>
2022-09-27 10:04:52 -0500

    Update

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -26,27 +26,13 @@ const Auth = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const { username, password, phoneNumber, avatarURL } = form;
+        const { fullName, username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'https://localhost:5000/auth';
-        // const URL = 'https://medical-pager.herokuapp.com/auth';
+        const URL = 'http://localhost:5000/auth';
 
-        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
-            username, password, fullName: form.fullName, phoneNumber, avatarURL,
-        });
-
-        cookies.set('token', token);
-        cookies.set('username', username);
-        cookies.set('fullName', fullName);
-        cookies.set('userId', userId);
-
-        if(isSignup) {
-            cookies.set('phoneNumber', phoneNumber);
-            cookies.set('avatarURL', avatarURL);
-            cookies.set('hashedPassword', hashedPassword);
-        }
-
-        window.location.reload();
+        const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL,
+        })
     }
 
     const switchMode = () => {
